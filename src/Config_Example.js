b1671aac89c6213970c5c4e55b90afef10872dc3
Shirodork <53873873+Shirodork@users.noreply.github.com>
2020-07-02 14:17:53 -0400

    Update Config_Example.js
    Added Comments

--- src/Config_Example.js
+++ src/Config_Example.js
@@ -1,18 +1,26 @@
+// Discord API Token (https://discord.com/developers)
 exports.TOKEN = 'DISCORD APP TOKEN HERE';
 
+// Bot Command Prefix
 exports.PREFIX = '!';
 
+// Google's YT API Key (https://console.developers.google.com/)
 exports.GOOGLE_API_KEY = 'GOOGLE YT DATA API KEY HERE';
 
+// Discord Server Guild ID
 exports.GUILDID = 'DISCORD SERVER ID';
 
+// Discord Server Channel to hold total user number statistic
 exports.TOTALUSERSID = 'CHANNEL TO KEEP TRACK OF TOTAL USER ID';
 
+// Discord Server Channel to hold memeber count (human)
 exports.MEMBERCOUNTID = 'CHANNEL TO KEEP TRACK OF MEMBER COUNT ID';
 
+// Discord Server Channel to hold total bot count
 exports.BOTCOUNTID = 'CHANNEL TO KEEP TRACK OF BOT COUNT ID';
 
+// The Discord ID of the bot owner
 exports.OWNERID = 'BOT OWNER DISCORD ID';
 
+// Discord Server's General Text Channel ID
 exports.GENERALCHANNEL = 'DISCORD SERVER GENERAL CHANNEL ID';
-
